dd545a6f38033b903308398579e29c5bceecf28d
limfluenza <limfluenza@naver.com>
2022-04-07 17:49:44 +0900

    format

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+const PORT = 4000;
+
+const app = express();
+
+const gossipMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("지나가도 됌");
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(gossipMiddleware);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
